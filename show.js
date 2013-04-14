ca199afaf35550286b54e95740ffad9ceeab394e
Victor Mak <victormakcy@gmail.com>
2013-04-14 15:39:30 -0400

    more changes

--- show.js
+++ show.js
@@ -26,15 +26,16 @@ function showTweets(result){
 
 	result.results.forEach(function(that){
 
-		var pictureURL = that.profile_image_url;
+		
 		var name = that.from_user;
+		var pictureURL = 'https://api.twitter.com/1/users/profile_image?screen_name=' + name + '&size=bigger';
 		var date = that.created_at;
 		var index = date.indexOf("+");
 		var message = that.text;
 
 		date = date.substr(0,index-1)
 
-		var addingTweet = '<li class="tweet-cards"><div id="profile-pic"><img src="' + pictureURL + '"/><p id="name"><a href="http://www.twitter.com/'+name+'" target="_blank">@'+ name +'</a></p><p id="date">' + date + '</p></div><div id="tweet">' + message + '</div></li>';
+		var addingTweet = '<li class="tweet-cards"><div class="card-top"></div><div class="profile-pic"><img src="' + pictureURL + '"/><p id="name"><a href="http://www.twitter.com/'+name+'" target="_blank">@'+ name +'</a></p><p id="date">' + date + '</p></div><div class="card-bottom"><div id="tweet">' + message + '</div></li>';
 
 		$('#stream').append(addingTweet);
 
