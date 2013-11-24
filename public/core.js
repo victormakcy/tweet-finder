20e6ff5d6973ae949ca8b445623260eab2501fd4
Victor Mak <victormakcy@gmail.com>
2013-11-23 22:27:51 -0500

    indentation

--- public/core.js
+++ public/core.js
@@ -2,28 +2,28 @@ var tweetFinder = angular.module('tweetFinderApp', []);
 
 function tweetFinderController($scope, $http) {
 
-	$scope.getTweets = function() {
-		// reset our tweets array every search
-		$scope.tweets = [];
+  $scope.getTweets = function() {
+    // reset our tweets array every search
+    $scope.tweets = [];
 
-		$http.get('/api/tweets?q=' + $scope.tweet.keyword)
-			.success($scope.displayTweets);
-	};
+    $http.get('/api/tweets?q=' + $scope.tweet.keyword)
+      .success($scope.displayTweets);
+  };
 
-	$scope.displayTweets = function(data) {
-		for (var i = 0; i < data.length; i++) {
-			var index = data[i].created_at.indexOf("+");
-			var date = data[i].created_at.substr(0,index-1)
+  $scope.displayTweets = function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var index = data[i].created_at.indexOf("+");
+      var date = data[i].created_at.substr(0,index-1)
 
-			// add each tweets info to tweets array
-			$scope.tweets.push({
-				"userProfileImageURL": (data[i].user.profile_image_url).replace("normal","bigger"),
-				"userScreenName": '@' + data[i].user.screen_name,
-				"userProfileURL": 'https://www.twitter.com/' + data[i].user.screen_name,
-				"message": data[i].text,
-				"date": date
-			});
-		}
+      // add each tweets info to tweets array
+      $scope.tweets.push({
+        "userProfileImageURL": (data[i].user.profile_image_url).replace("normal","bigger"),
+        "userScreenName": '@' + data[i].user.screen_name,
+        "userProfileURL": 'https://www.twitter.com/' + data[i].user.screen_name,
+        "message": data[i].text,
+        "date": date
+      });
+    }
 
-	}
+  }
 };
